5a6c5b0d6026607fd6f96c98bf201861d2e0d22c
aytanumudova <aytanumudova@gmail.com>
2023-11-27 21:53:08 +0400

    project was done

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const card = ({ dataTitle, dataText, dataImg }) => {
-  return (
-    <div className="col-12 col-md-6 col-lg-3 text-center mt-5 mt-lg-0 custom-card">
-      <h5>{dataTitle}</h5>
-      <p className="card-text text-secondary my-3 mb-4">{dataText}</p>
-      <img src={dataImg} alt="cardImg" />
-    </div>
-  );
+const card = ({dataTitle, dataText, dataImg}) => {
+    return (
+        <div className="custom-card d-flex flex-column justify-content-center align-items-center">
+            <h5>{dataTitle}</h5>
+            <p className="card-text text-secondary my-3 mb-4">{dataText}</p>
+            <img src={dataImg} alt="cardImg" className="m-auto"/>
+        </div>
+    );
 };
 
 export default card;
--- src/components/cardsSection.jsx
+++ src/components/cardsSection.jsx
@@ -9,33 +9,32 @@ import dataBase from "../data.json";
 
 const cardsSection = () => {
   return (
-    <div className="cardsSection row container m-auto py-5 my-5">
+    <div className="cardsSection container py-5 my-5">
       <Swiper
-        loop={true}
+        modules={[Autoplay, Pagination]}
         autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
+            delay: 3500,
+            disableOnInteraction: true,
         }}
-        spaceBetween={10}
         pagination={{
-          clickable: true,
+            clickable: true,
         }}
         breakpoints={{
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          600: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1000: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            600: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1000: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
         }}
         modules={[Autoplay, Pagination]}
-        className="pb-5"
+        className="pb-5 mb-5"
       >
         {dataBase.map((data) => (
           <SwiperSlide>
